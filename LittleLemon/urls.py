8eceb889820c8bfca7b9ac6e609ed2910e4d9986
Zarrar <69991738+zahmad156@users.noreply.github.com>
2023-02-24 20:37:52 +0500

    Update urls.py

--- LittleLemon/urls.py
+++ LittleLemon/urls.py
@@ -24,7 +24,7 @@ router.register(r'tables', views.BookingViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('restaurant.urls')),
+    path('restaurant/', include('restaurant.urls')),
     path('restaurant/booking/', include(router.urls)),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
